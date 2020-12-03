57581b1948bfa89a9cf2a10374dc889dbc33c1f0
EClaesson <emanuel.claesson@gmail.com>
2020-12-03 06:25:40 +0100

    2020 - 3

---
+++ 2020/3/aoc_2020_3.py
@@ -0,0 +1,35 @@
+import functools
+
+
+def get_at(input_line, index):
+  return input_line[index % len(input_line)]
+
+
+def get_tree_count(lines, x_delta, y_delta):
+  tree_count = 0
+  x = 0
+  y = y_delta
+
+  while y < len(lines):
+    x += x_delta
+
+    if get_at(lines[y], x) == '#':
+      tree_count += 1
+
+    y += y_delta
+
+  return tree_count
+
+
+def run_part_a(input_lines):
+  return get_tree_count(input_lines, 3, 1)
+
+
+def run_part_b(input_lines):
+  return functools.reduce(lambda a, b: a * b, [get_tree_count(input_lines, slope[0], slope[1]) for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
+
+
+if __name__ == '__main__':
+  with open('input') as input_file:
+      input_lines = [line.strip('\n') for line in input_file.readlines()]
+      print(f'Part A: {run_part_a(input_lines)}\nPart B: {run_part_b(input_lines)}')
