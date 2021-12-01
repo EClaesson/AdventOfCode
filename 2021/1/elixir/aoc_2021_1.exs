397625488d5d3419c77730d05c7a42a7a82578a1
Emanuel Claesson <emanuel.claesson@gmail.com>
2021-12-02 00:38:42 +0100

    2021, Day 1, Elixir - Cleanup

--- 2021/1/elixir/aoc_2021_1.exs
+++ 2021/1/elixir/aoc_2021_1.exs
@@ -1,19 +1,20 @@
 defmodule AOC do
   def read_input() do
     File.stream!("../input")
-    |> Stream.map(fn (line) -> line |> Integer.parse |> elem(0) end)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.to_integer/1)
     |> Enum.to_list
   end
 
   def run_part_a(values) do
-    1..(length(values) - 1)
-    |> Enum.filter(fn index -> Enum.at(values, index) > Enum.at(values, index - 1) end)
-    |> length
+    values
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a, b] -> b > a end)
   end
 
   def run_part_b(values) do
     Enum.chunk_every(values, 3, 1, :discard)
-    |> Enum.map(fn chunk -> chunk |> Enum.sum end)
+    |> Enum.map(&Enum.sum/1)
     |> run_part_a
   end
 end
